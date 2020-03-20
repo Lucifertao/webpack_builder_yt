77460e516d8989c8f2be8c5cba26edaf78f8e069
Lucifertao <try_yang@163.com>
2020-03-20 18:21:32 +0800

    fix: 🐛 优化单元测试

--- test/unit/webpack.base.spec.js
+++ test/unit/webpack.base.spec.js
@@ -1,4 +1,10 @@
+beforeAll(() => {
+    const path = require('path')
+    const templatePath = path.join(process.cwd(), './test/smoke/template');
+    process.chdir(templatePath)
+});
 test('webpack.base.js test case', () => {
+    console.log(process.cwd())
     const baseConfig = require('../../lib/webpack.base')
-    expect(baseConfig.entry.index).toContain('/src/index/index.js')
+    expect(baseConfig.entry.index).toContain('test/smoke/template/src/index/index.js')
 });
--- test/unit/webpack.prod.spec.js
+++ test/unit/webpack.prod.spec.js
@@ -1,10 +1,14 @@
-test('webpack.prod.js test case', () => {
+beforeAll(() => {
     const path = require('path')
     const templatePath = path.join(process.cwd(), './test/smoke/template');
     process.chdir(templatePath)
+});
+test('webpack.prod.js test case', () => {
+    const path = require('path')
+    const templatePath = process.cwd();
     const baseConfig = require('../../lib/webpack.prod')
     const { version } = require(path.join(templatePath, './package.json'))
-    expect(baseConfig.output.path).toContain(`/dist/${version}`)
+    expect(baseConfig.output.path).toContain(`test/smoke/template/dist/${version}`)
     expect(baseConfig.devtool).toEqual('none')
     expect(baseConfig.mode).toEqual('production')
 });
